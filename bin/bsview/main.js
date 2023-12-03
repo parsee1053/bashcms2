fc7e0284974fa2fdbe9c67eb9d454a80114a7cd0
parsee1053 <s.naiki.parsee1053@gmail.com>
2023-12-04 02:58:07 +0900

    jsのリファクタリング

--- bin/bsview/main.js
+++ bin/bsview/main.js
@@ -1,65 +1,48 @@
 window.onload = function () {
-    lastArticles(10);
-    rankArticles(10);
-    linkKeywords();
-//    fullSearch("");
+  lastArticles(10);
+  rankArticles(10);
+  linkKeywords();
 }
 
 function lastArticles(num) {
-    var httpReq = new XMLHttpRequest();
-    httpReq.onreadystatechange = function () {
-        if (httpReq.readyState != 4 || httpReq.status != 200) {
-            return;
-        }
-        document.getElementById("last-articles").innerHTML = httpReq.responseText;
-    }
-    var url = "/last_articles.cgi?num=" + num;
-    httpReq.open("GET", url, true);
-    httpReq.send(null);
+  const url = "/last_articles.cgi?num=" + num;
+  fetch(url, { method: "GET" })
+    .then(response => response.text())
+    .then((text) => {
+      document.getElementById("last-articles").innerHTML = text;
+    });
 }
 
 function linkKeywords() {
-    var httpReq = new XMLHttpRequest();
-    httpReq.onreadystatechange = function () {
-        if (httpReq.readyState != 4 || httpReq.status != 200) {
-            return;
-        }
-        document.getElementById("keywords").innerHTML = httpReq.responseText;
-    }
-    var word = document.getElementById("keywords").innerHTML;
-    var url = "/link_keywords.cgi?keywords=" + encodeURIComponent(word);
-    httpReq.open("GET", url, true);
-    httpReq.send(null);
+  const word = document.getElementById("keywords").innerHTML;
+  const url = "/link_keywords.cgi?keywords=" + encodeURIComponent(word);
+  fetch(url, { method: "GET" })
+    .then(response => response.text())
+    .then((text) => {
+      document.getElementById("keywords").innerHTML = text;
+    });
 }
 
 function fullSearch() {
-    var word = document.getElementById("full-search-box").value;
-    if (word == "") {
-        return;
-    }
-    var httpReq = new XMLHttpRequest();
-    httpReq.onreadystatechange = function () {
-        if (httpReq.readyState != 4 || httpReq.status != 200) {
-            return;
-        }
-        document.getElementById("article-body").innerHTML = httpReq.responseText;
-        document.body.style.cursor = "default";
-    }
-    var url = "/bsview/full_search.cgi?word=" + encodeURIComponent(word);
-    httpReq.open("GET", url, true);
-    httpReq.send(null);
-    document.body.style.cursor = "wait";
+  const word = document.getElementById("full-search-box").value;
+  if (word == "") {
+    return;
+  }
+  document.body.style.cursor = "wait";
+  const url = "/bsview/full_search.cgi?word=" + encodeURIComponent(word);
+  fetch(url, { method: "GET" })
+    .then(response => response.text())
+    .then((text) => {
+      document.getElementById("article-body").innerHTML = text;
+      document.body.style.cursor = "default";
+    });
 }
 
 function rankArticles(num) {
-    var httpReq = new XMLHttpRequest();
-    httpReq.onreadystatechange = function () {
-        if (httpReq.readyState != 4 || httpReq.status != 200) {
-            return;
-        }
-        document.getElementById("rank-articles").innerHTML = httpReq.responseText;
-    }
-    var url = "/rank_articles.cgi?num=" + num;
-    httpReq.open("GET", url, true);
-    httpReq.send(null);
+  const url = "/rank_articles.cgi?num=" + num;
+  fetch(url, { method: "GET" })
+    .then(response => response.text())
+    .then((text) => {
+      document.getElementById("rank-articles").innerHTML = text;
+    });
 }
